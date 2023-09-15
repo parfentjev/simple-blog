560de00f980468de41f1c659a47880d7f24357f4
Aleksei Parfentjev <aleksei.parfentjev@proton.me>
2023-09-15 22:20:00 +0300

    fix build

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,6 +2,7 @@ import Layout from '@/ui/layout/Layout'
 import { AppProps } from 'next/app'
 import './../src/ui/layout/common.css'
 import Head from 'next/head'
+import Script from 'next/script'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
@@ -18,12 +19,12 @@ const App = ({ Component, pageProps }: AppProps) => {
         <link rel='apple-touch-icon' href='logo192.png' />
         <link rel='manifest' href='manifest.json' />
         <title>Fake Plastic Trees</title>
-        <script
-          src='https://cdn.counter.dev/script.js'
-          data-id='ad8cfdc0-03e1-4bc9-9944-56c71e54a8c9'
-          data-utcoffset='3'
-        ></script>
       </Head>
+      <Script
+        src='https://cdn.counter.dev/script.js'
+        data-id='ad8cfdc0-03e1-4bc9-9944-56c71e54a8c9'
+        data-utcoffset='3'
+      ></Script>
       <Layout>
         <Component {...pageProps} />
       </Layout>
