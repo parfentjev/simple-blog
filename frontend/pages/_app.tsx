691256a4a7035ba430f210958e48b292f37cfa4d
Aleksei Parfentjev <aleksei.parfentjev@proton.me>
2023-10-31 15:44:15 +0200

    colorize code blocks, add scrollbars

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -9,15 +9,15 @@ const App = ({ Component, pageProps }: AppProps) => {
     <>
       <Head>
         <meta charSet='utf-8' />
-        <link rel='icon' href='favicon.ico' />
+        <link rel='icon' href='/favicon.ico' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta name='theme-color' content='#000000' />
         <meta
           name='description'
           content='A simple blog about technical topics, life and other things.'
         />
-        <link rel='apple-touch-icon' href='logo192.png' />
-        <link rel='manifest' href='manifest.json' />
+        <link rel='apple-touch-icon' href='/logo192.png' />
+        <link rel='manifest' href='/manifest.json' />
         <title>Fake Plastic Trees</title>
       </Head>
       <Script
