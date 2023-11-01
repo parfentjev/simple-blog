e2f3b7512b6e150757fc2baddf6ba6adf7a29196
Aleksei Parfentjev <aleksei.parfentjev@proton.me>
2023-11-01 21:13:56 +0200

    fix issue revealed by tests

--- backend/src/main/java/ee/fakeplastictrees/blog/user/controller/request/PostUsersRequest.java
+++ backend/src/main/java/ee/fakeplastictrees/blog/user/controller/request/PostUsersRequest.java
@@ -3,13 +3,13 @@ package ee.fakeplastictrees.blog.user.controller.request;
 import ee.fakeplastictrees.blog.user.model.UserRole;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class PostUsersRequest {
     @NotBlank
