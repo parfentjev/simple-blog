f78d2de419e99e8cbb0050f6728692caace15576
Aleksei Parfentjev <aleksei.parfentjev@proton.me>
2023-11-05 00:00:19 +0200

    generate rss feed

---
+++ backend/src/main/java/ee/fakeplastictrees/blog/core/configuration/JobScheduler.java
@@ -0,0 +1,91 @@
+package ee.fakeplastictrees.blog.core.configuration;
+
+import com.rometools.rome.feed.synd.SyndContentImpl;
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndEntryImpl;
+import com.rometools.rome.feed.synd.SyndFeedImpl;
+import com.rometools.rome.io.SyndFeedOutput;
+import ee.fakeplastictrees.blog.post.model.PostPreviewDto;
+import ee.fakeplastictrees.blog.post.service.PostService;
+import lombok.AccessLevel;
+import lombok.SneakyThrows;
+import lombok.experimental.FieldDefaults;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.io.FileWriter;
+import java.util.List;
+
+import static java.lang.String.format;
+
+@Configuration
+@EnableScheduling
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class JobScheduler {
+    final Logger logger = LoggerFactory.getLogger(JobScheduler.class);
+
+    @Autowired
+    PostService postService;
+
+    @Value("${rss.feed.outputFile}")
+    String rssFeedOutputFile;
+
+    @Value("${rss.feed.type}")
+    String rssFeedType;
+
+    @Value("${rss.feed.title}")
+    String rssFeedTitle;
+
+    @Value("${rss.feed.description}")
+    String rssFeedDescription;
+
+    @Value("${rss.feed.websiteLink}")
+    String rssFeedWebsiteLink;
+
+    @Value("${rss.feed.linkFormat}")
+    String rssFeedLinkFormat;
+
+    @Value("${rss.feed.items}")
+    Integer rssFeedItems;
+
+    @SneakyThrows
+    @Scheduled(cron = "${rss.feed.cronExpression}")
+    public void generateRssFeed() {
+        List<SyndEntry> entries = postService.getPosts(1, rssFeedItems)
+                .getItems()
+                .stream()
+                .map(this::mapPostToEntry)
+                .toList();
+
+        SyndFeedImpl feed = new SyndFeedImpl();
+        feed.setFeedType(rssFeedType);
+        feed.setTitle(rssFeedTitle);
+        feed.setDescription(rssFeedDescription);
+        feed.setLink(rssFeedWebsiteLink);
+        feed.setEntries(entries);
+
+        try (FileWriter writer = new FileWriter(rssFeedOutputFile)) {
+            new SyndFeedOutput().output(feed, writer);
+        }
+
+        logger.info("Updated the RSS feed: " + rssFeedOutputFile);
+    }
+
+    private SyndEntry mapPostToEntry(PostPreviewDto post) {
+        SyndContentImpl description = new SyndContentImpl();
+        description.setValue(post.getSummary());
+
+        SyndEntry entry = new SyndEntryImpl();
+        entry.setUri(post.getId());
+        entry.setTitle(post.getTitle());
+        entry.setDescription(description);
+        entry.setLink(format(rssFeedLinkFormat, post.getId()));
+
+        return entry;
+    }
+}
