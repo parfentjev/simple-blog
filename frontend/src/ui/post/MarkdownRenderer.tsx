25dce64479692a80c40e4dcd7aef134873b76841
Aleksei Parfentjev <aleksei.parfentjev@proton.me>
2023-11-06 22:09:40 +0200

    add code highlighting

--- frontend/src/ui/post/MarkdownRenderer.tsx
+++ frontend/src/ui/post/MarkdownRenderer.tsx
@@ -1,8 +1,36 @@
 import { FC } from 'react'
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import tomorrow from 'react-syntax-highlighter/dist/esm/styles/prism/tomorrow'
 
 const MarkdownRenderer: FC<{ text: string }> = ({ text }) => {
-  return <ReactMarkdown linkTarget={'_blank'}>{text}</ReactMarkdown>
+  return (
+    <>
+      <ReactMarkdown
+        linkTarget={'_blank'}
+        components={{
+          code(props) {
+            const { children, className, node, inline, ...rest } = props
+            const match = /language-(\w+)/.exec(className || '')
+
+            return inline ? (
+              <code>{children}</code>
+            ) : (
+              <SyntaxHighlighter
+                {...rest}
+                PreTag='div'
+                children={String(children).replace(/\n$/, '')}
+                language={match != null ? match[1] : ''}
+                style={tomorrow}
+              />
+            )
+          },
+        }}
+      >
+        {text}
+      </ReactMarkdown>
+    </>
+  )
 }
 
 export default MarkdownRenderer
