8deb3bd9ea05d67f8adffb7f51607eab077d045b
Aleksei Parfentjev <aleksei.parfentjev@protonmail.com>
2024-05-03 22:10:05 +0300

    add pagination to the web app

--- webapp/src/routes/pages/posts/PostsPage.tsx
+++ webapp/src/routes/pages/posts/PostsPage.tsx
@@ -3,22 +3,30 @@ import { toast } from 'react-toastify'
 import PostsList from '../../../ui/posts/PostsList'
 import { PagePostDto } from '../../../api/codegen'
 import { postsApi } from '../../../api/api'
+import Paginator from '../../../ui/core/Paginator'
+import { useParams } from 'react-router-dom'
 
 const PostsPage: FC = () => {
+    const { page: pageNumber } = useParams()
     const [page, setPage] = useState<PagePostDto>()
 
     useEffect(() => {
         postsApi()
-            .postsPublishedGet()
+            .postsPublishedGet({ page: Number(pageNumber || 1) })
             .then((page) => setPage(page))
             .catch(() => toast.error('Failed to laod posts.'))
-    }, [])
+    }, [pageNumber])
 
     if (!page || page.items.length === 0) {
         return <p className="text-center">There are no posts yet!</p>
     }
 
-    return <PostsList posts={page.items} />
+    return (
+        <>
+            <PostsList posts={page.items} />
+            <Paginator page={page.page} totalPages={page.totalPages} />
+        </>
+    )
 }
 
 export default PostsPage
