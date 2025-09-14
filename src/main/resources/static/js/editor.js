c11d57bef96b6220e39312def83d7da95e0cff35
Aleksei Parfentjev <aleksei.parfentjev@tutamail.com>
2025-09-14 11:35:12 +0300

    update editor.js to prevent false beforeunload alerts

--- src/main/resources/static/js/editor.js
+++ src/main/resources/static/js/editor.js
@@ -1,1 +1,10 @@
-window.addEventListener('beforeunload', (e) => e.preventDefault())
+let formModified = false;
+
+['input', 'textarea'].flatMap((tag) => Array.from(document.getElementsByTagName(tag)))
+  .forEach((element) => element.addEventListener('input', (e) => { formModified = true; }));
+
+window.addEventListener('beforeunload', (e) => {
+  if (!formModified || e.explicitOriginalTarget.type === 'submit') return;
+
+  e.preventDefault();
+})
