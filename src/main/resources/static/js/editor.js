1c8d6be27b9996c81083f117ec6b3535efd7073c
Aleksei Parfentjev <aleksei.parfentjev@tutamail.com>
2025-11-17 21:19:29 +0200

    add menu to admin panel; make textarea collapsable

--- src/main/resources/static/js/editor.js
+++ src/main/resources/static/js/editor.js
@@ -8,3 +8,16 @@ window.addEventListener('beforeunload', (e) => {
 
   e.preventDefault();
 })
+
+const controlElementRows = (name, sizes) => {
+  const element = document.getElementsByName(name)[0];
+  if (!element) return
+
+  let cursor = 0;
+  element.addEventListener('dblclick', () => {
+    element.rows = sizes[++cursor % sizes.length]
+  })
+}
+
+controlElementRows('summary', [5, 25]);
+controlElementRows('text', [25, 5]);
