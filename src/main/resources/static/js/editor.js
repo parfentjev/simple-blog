094bd18e869726ee445ca7368e2b0b3849e5b4fa
Aleksei Parfentjev <aleksei.parfentjev@tutamail.com>
2025-12-19 23:40:06 +0200

    remove inline script

--- src/main/resources/static/js/editor.js
+++ src/main/resources/static/js/editor.js
@@ -1,23 +1,26 @@
-let formModified = false;
-
-['input', 'textarea'].flatMap((tag) => Array.from(document.getElementsByTagName(tag)))
-  .forEach((element) => element.addEventListener('input', (e) => { formModified = true; }));
+let formModified = false[('input', 'textarea')]
+  .flatMap((tag) => Array.from(document.getElementsByTagName(tag)))
+  .forEach((element) =>
+    element.addEventListener('input', (e) => {
+      formModified = true
+    })
+  )
 
 window.addEventListener('beforeunload', (e) => {
-  if (!formModified || e.explicitOriginalTarget.type === 'submit') return;
+  if (!formModified || e.explicitOriginalTarget.type === 'submit') return
 
-  e.preventDefault();
+  e.preventDefault()
 })
 
 const controlElementRows = (name, sizes) => {
-  const element = document.getElementsByName(name)[0];
+  const element = document.getElementsByName(name)[0]
   if (!element) return
 
-  let cursor = 0;
+  let cursor = 0
   element.addEventListener('dblclick', () => {
     element.rows = sizes[++cursor % sizes.length]
   })
 }
 
-controlElementRows('summary', [5, 25]);
-controlElementRows('text', [25, 5]);
+controlElementRows('summary', [5, 25])
+controlElementRows('text', [25, 5])
